245f7d807c76dec9140dbdef991e55404ecb022d
Carlos Eduardo <ocarlls.eduardo@gmail.com>
2023-11-07 15:28:18 -0300

    Estruturação da página inicial onde será criado o formulario de cadastro

---
+++ lib/app/pages/app_widget.dart
@@ -0,0 +1,16 @@
+import 'package:crud_flutter/app/pages/register/register_page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      title: "Flutter Demo",
+      home: RegisterPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
