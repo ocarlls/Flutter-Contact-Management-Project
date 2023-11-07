89467d3fd4311d75b600b4e1844bf8571c9e89f1
Carlos Eduardo <ocarlls.eduardo@gmail.com>
2023-11-07 16:01:14 -0300

    Estrutura o formulário de cadastro

--- lib/app/pages/register/register_page.dart
+++ lib/app/pages/register/register_page.dart
@@ -8,11 +8,55 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController nomeController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController telefoneController = TextEditingController();
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Cadastro'),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.all(5),
+        child: FractionallySizedBox(
+          widthFactor: 0.9,
+          heightFactor: 0.9,
+          child: ListView(
+            children: [
+              Text('Nome'),
+              SizedBox(height: 5),
+              TextField(
+                controller: nomeController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Justin Case'
+                  ),
+              ),
+              Text('Email'),
+              SizedBox(height: 5),
+              TextField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'justincase@gmail.com'
+                  ),
+              ),
+              Text('Telefone'),
+              SizedBox(height: 5),
+              TextField(
+                controller: telefoneController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: '555-555-555'
+                  ),
+              ),
+              ElevatedButton(onPressed: (){}, child: Text('Cadastrar-se')),
+            ],
+          ),
+        ),
       ),
     );
   }
