5e38577d068e0c4edb8249509c94f0edb1b45d49
Carlos Eduardo <ocarlls.eduardo@gmail.com>
2023-11-08 08:12:50 -0300

    cria rota post de usuario

--- BackEnd/Flutter/src/main/java/com/interagindo/Flutter/controllers/UsuarioController.java
+++ BackEnd/Flutter/src/main/java/com/interagindo/Flutter/controllers/UsuarioController.java
@@ -1,2 +1,19 @@
-package com.interagindo.Flutter.controllers;public class UsuarioController {
+package com.interagindo.Flutter.controllers;
+
+import com.interagindo.Flutter.models.Usuario;
+import com.interagindo.Flutter.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/usuario")
+@CrossOrigin
+public class UsuarioController {
+    @Autowired
+    UsuarioService service;
+    @PostMapping("/cadastrar")
+    private ResponseEntity cadastrarUsuario(@RequestBody Usuario usuario){
+        return service.save(usuario);
+    }
 }
--- BackEnd/Flutter/src/main/java/com/interagindo/Flutter/services/UsuarioService.java
+++ BackEnd/Flutter/src/main/java/com/interagindo/Flutter/services/UsuarioService.java
@@ -1,2 +1,19 @@
-package com.interagindo.Flutter.services;public class UsuarioService {
+package com.interagindo.Flutter.services;
+
+import com.interagindo.Flutter.models.Usuario;
+import com.interagindo.Flutter.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+    @Autowired
+    UsuarioRepository repository;
+
+    public ResponseEntity save(Usuario usuario){
+        repository.save(usuario);
+        return ResponseEntity.ok().body("Usuario salvo com sucesso!");
+    }
+
 }
